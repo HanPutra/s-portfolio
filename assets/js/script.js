5b21ec0ae3c49215cf3c29b9782e42371b14a7e5
handika putra <moch.handika.putra.i@gmail.com>
2024-06-04 21:16:42 +0700

    update header nav hamburger about for tablet and add hamburger function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 // author: Handika Putra
 // github account: https://github.com/HanPutra
+
+// hamburger
+const hamburger = document.querySelector(".hamburger-icon");
+const nav = document.querySelector("nav");
+hamburger.addEventListener("click", function () {
+  nav.classList.toggle("show");
+  hamburger.classList.toggle("active");
+});
